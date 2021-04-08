14a0ef287039c5ee7287c72eee14fc2ab5636e1a
estradahamma22 <56485389+estradahamma22@users.noreply.github.com>
2021-04-07 20:51:26 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -34,115 +34,25 @@ from scipy import signal
 from scipy.io import wavfile
 
 
-data_dir = pathlib.Path('data/mini_speech_commands')
-if not data_dir.exists():
-  tf.keras.utils.get_file(
-      'mini_speech_commands.zip',
-      origin="http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip",
-      extract=True,
-      cache_dir='.', cache_subdir='data')
-
-commands = np.array(tf.io.gfile.listdir(str(data_dir)))
-commands = commands[commands != 'README.md']
-print('Commands:', commands)
-
-filenames = tf.io.gfile.glob(str(data_dir) + '/*/*')
-filenames = tf.random.shuffle(filenames)
-num_samples = len(filenames)
-print('Number of total examples:', num_samples)
-print('Number of examples per label:',
-      len(tf.io.gfile.listdir(str(data_dir/commands[0]))))
-print('Example file tensor:', filenames[0])
-
-train_files = filenames[:6400]
-val_files = filenames[6400: 6400 + 800]
-test_files = filenames[-800:]
-
-print('Training set size', len(train_files))
-print('Validation set size', len(val_files))
-print('Test set size', len(test_files))
-
-def get_label(file_path):
-  parts = tf.strings.split(file_path, os.path.sep)
-
-  # Note: You'll use indexing here instead of tuple unpacking to enable this
-  # to work in a TensorFlow graph.
-  return parts[-2]
-def decode_audio(audio_binary):
-  audio, _ = tf.audio.decode_wav(audio_binary)
-  return tf.squeeze(audio, axis=-1)
-
-def get_waveform_and_label(file_path):
-  label = get_label(file_path)
-  audio_binary = tf.io.read_file(file_path)
-  waveform = decode_audio(audio_binary)
-  return waveform, label
-
-AUTOTUNE = tf.data.AUTOTUNE
-files_ds = tf.data.Dataset.from_tensor_slices(train_files)
-waveform_ds = files_ds.map(get_waveform_and_label, num_parallel_calls=AUTOTUNE)
-
-def get_spectrogram(waveform):
-  # Padding for files with less than 16000 samples
-  zero_padding = tf.zeros([16000] - tf.shape(waveform), dtype=tf.float32)
-
-  # Concatenate audio with padding so that all audio clips will be of the
-  # same length
-  waveform = tf.cast(waveform, tf.float32)
-  equal_length = tf.concat([waveform, zero_padding], 0)
-  spectrogram = tf.signal.stft(
-      equal_length, frame_length=255, frame_step=128)
-
-  spectrogram = tf.abs(spectrogram)
-
-  return spectrogram
-
-for waveform, label in waveform_ds.take(1):
-  label = label.numpy().decode('utf-8')
-  spectrogram = get_spectrogram(waveform)
-
-print('Label:', label)
-print('Waveform shape:', waveform.shape)
-print('Spectrogram shape:', spectrogram.shape)
-print('Audio playback')
-display.display(display.Audio(waveform, rate=16000))
-
-def plot_spectrogram(spectrogram, ax):
-  # Convert to frequencies to log scale and transpose so that the time is
-  # represented in the x-axis (columns).
-  log_spec = np.log(spectrogram.T)
-  height = log_spec.shape[0]
-  width = log_spec.shape[1]
-  X = np.linspace(0, np.size(spectrogram), num=width, dtype=int)
-  Y = range(height)
-  ax.pcolormesh(X, Y, log_spec)
-
-
-fig, axes = plt.subplots(2, figsize=(12, 8))
-timescale = np.arange(waveform.shape[0])
-axes[0].plot(timescale, waveform.numpy())
-axes[0].set_title('Waveform')
-axes[0].set_xlim([0, 16000])
-plot_spectrogram(spectrogram.numpy(), axes[1])
-axes[1].set_title('Spectrogram')
-plt.show()
-
-
 """Easier way to preprocess the audio requires the download of the files
 https://medium.com/@nitinsingh1789/spoken-digit-classification-b22d67fd24b0
 this will take all the files I think in any format and makes them into stft"""
-
+#needs to be the path to the file of recording
 file = os.listdir('free-spoken-digit-dataset/recordings')
 data=[]
+#needs to go through the 
 for i in file:
     x , sr = librosa.load('free-spoken-digit-dataset/recordings/'+i)
     data.append(x)
-
+#X is were the spectrograms are stored
 X=[]
 for i in range(len(data)):
     X.append(abs(librosa.stft(data[i]).mean(axis = 1).T))
 X= np.array(X)
 y= []
+#assigning the label 1 for real audio
+#use zero for fake audio
 for i in range(len(data)):
     y.append(1)
 y = mp.array(y)
+X.to_csv("spectrogram",index = False)
