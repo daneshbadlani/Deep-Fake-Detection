b6c2492aae0ae2c57f150b67f15765d7f5788bd3
estradahamma22 <56485389+estradahamma22@users.noreply.github.com>
2021-04-15 13:44:43 -0400

    Add in the calls for the data
    need to have both datasets downloaded

--- CNN_Assignment.py
+++ CNN_Assignment.py
@@ -12,9 +12,64 @@ from tensorflow.keras import optimizers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.utils import to_categorical
 
-## Load is Cifar 10 dataset
-(X_train, y_train), (X_test, y_test) = #IMAGE DATA
+"""IMAGE PREPROCESSING"""
+PIC_DIR = './celeba_gan/img_align_celeba/'
+IMAGES_COUNT = 10000
+ORIG_WIDTH = 178
+ORIG_HEIGHT = 208
+diff = (ORIG_HEIGHT - ORIG_WIDTH) // 2
+WIDTH = 128
+HEIGHT = 128
+crop_rect = (0, diff, ORIG_WIDTH, ORIG_HEIGHT - diff)
+realImages = []
+for imageName in tqdm(os.listdir(PIC_DIR)[IMAGES_COUNT:20000]):
+    pic = Image.open(PIC_DIR + imageName).crop(crop_rect)
+    pic.thumbnail((WIDTH, HEIGHT), Image.ANTIALIAS)
+    realImages.append(np.uint8(pic))  # Normalize the images
+realImages = np.array(realImages) / 255
+print("Images Shape:", realImages.shape)
+plt.figure(1, figsize=(10, 10))
 
+# Saving sample 25 images to see what faces look like
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.imshow(realImages[i])
+    # plt.axis('off')
+plt.savefig('./test')
+"""IMAGE PREPROCESSING"""
+PIC_DIR = './fakeImages30000/'
+IMAGES_COUNT = 10000
+ORIG_WIDTH = 178
+ORIG_HEIGHT = 208
+diff = (ORIG_HEIGHT - ORIG_WIDTH) // 2
+WIDTH = 128
+HEIGHT = 128
+crop_rect = (0, diff, ORIG_WIDTH, ORIG_HEIGHT - diff)
+images = []
+for imageName in tqdm(os.listdir(PIC_DIR)[:10000]):
+    pic = Image.open(PIC_DIR + imageName)
+    pic.thumbnail((WIDTH, HEIGHT), Image.ANTIALIAS)
+    images.append(np.uint8(pic))  # Normalize the images
+images = np.array(fakeImages) / 255
+print("Images Shape:", fakeImages.shape)
+plt.figure(1, figsize=(10, 10))
+
+# Saving sample 25 images to see what faces look like
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.imshow(fakeImages[i])
+    # plt.axis('off')
+plt.savefig('./test1')
+
+## Load is Cifar 10 dataset
+dataSet = np.concatenate([images, realImages], axis =0)
+y_values = []
+for i in range(0,10000):
+  y_values[i] = 0
+for i in range(10001, 20000):
+  y_values[i] = 1
+(X_train, y_train), (X_test, y_test) = train_test_split(dataSet, y_values,
+                                    random_state = 0.42, test_size = 0.1 )
 ## Print out shapes of training and testing sets
 print(X_train.shape)
 print(X_test.shape)
